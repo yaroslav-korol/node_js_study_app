c9a2e6fe5897fe39380b734a099f97c76e42c3e2
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2024-04-09 23:24:14 +0200

    Add the rest of ejs files

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ app.set('view engine', 'ejs');
 // listen for requests on port 3000
 app.listen(3000);
 
-// default route
+// home page
 app.get('/', (req, res) => {
-    res.render('<p>Hello World!</p>');
+    res.render('index');
 });
 
-// about
+// about page
 app.get('/about', (req, res) => {
-    res.send('<p>About Me</p>');
+    res.render('about');
+});
+
+
+// redirects example
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// create a new blog page
+app.get('/blogs/create', (req, res) => {
+    res.render('create.ejs');
+});
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404');
 });
